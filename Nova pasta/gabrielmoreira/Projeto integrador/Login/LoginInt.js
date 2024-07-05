39830593ac12b742db9f0273a39e6c93fab2f10d
Gabriel Moreira <133922923+gmoreira2128@users.noreply.github.com>
2024-07-05 00:34:03 -0300

    Update LoginInt.js

--- Nova pasta/gabrielmoreira/Projeto integrador/Login/LoginInt.js
+++ Nova pasta/gabrielmoreira/Projeto integrador/Login/LoginInt.js
@@ -1,39 +1,18 @@
-document.getElementById('btn-menu').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle("active");
-});
 
-window.onclick = function(event) {
-    if (!event.target.closest('#sidebar') && !event.target.closest('#btn-menu')) {
-        var dropdowns = document.getElementsByClassName('sidebar');
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('active')) {
-                openDropdown.classList.remove('active'); 
-            }
-        }
-    }
-};
+document.getElementById('login').addEventListener('click', function() {
+    var emailDigitado = document.getElementById('email-login').value;
+    var senhaDigitada = document.getElementById('senha-login').value;
 
+    var emailPadrao = "exemplo@email.com";
+    var senhaPadrao = "senha123";
 
-document.getElementById('login').addEventListener('click', function(){
-
-    email1 = document.getElementById('email-login').value;
-    senha1 = document.getElementById('senha-login').value;
-
-    EmailPadrao = "exemplo@email.com";
-    SenhaPadrao = "senha123";
-
-    if(email1 === EmailPadrao && senha1 === SenhaPadrao){
+    if (emailDigitado === emailPadrao && senhaDigitada === senhaPadrao) {
         console.log("Login realizado");
-        mensagem1 = document.getElementById('mensagem-sucesso').innerHTML = "Login realizado com sucesso";
-        mensagem1.style.color = 'green';
-    }
-    else{
+        document.getElementById('mensagem-sucesso').innerHTML = "Login realizado com sucesso";
+        document.getElementById('mensagem-falha').innerHTML = ""; // Limpa a mensagem de falha se houver
+    } else {
         console.log("Login não realizado");
-    mensagem2 = document.getElementById('mensagem-falha').innerHTML = "Login ou senha invalido";
-    mensagem2.style.color = 'red';
+        document.getElementById('mensagem-sucesso').innerHTML = ""; // Limpa a mensagem de sucesso se houver
+        document.getElementById('mensagem-falha').innerHTML = "Login ou senha inválidos";
     }
-
 });
-
