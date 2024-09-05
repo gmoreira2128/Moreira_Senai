b434202697ad98ded8c532b878321d1b43e15f5a
Gabriel Moreira <133922923+gmoreira2128@users.noreply.github.com>
2024-09-05 17:08:17 -0300

    Add files via upload

---
+++ Listas de Exercicios/atividade em c/arquivo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void Soma(int a, int b, int resultado)
+{
+
+    printf("Digite os valores da Soma: ");
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    resultado = a + b;
+    
+}
+
+void Subtracao(int aSub, int bSub, int resultadoSub)
+{
+    
+}
+
+int main()
+{
+    
+}
